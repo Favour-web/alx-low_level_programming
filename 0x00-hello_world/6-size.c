cebd7eabc5c7206d9d7677d122296cd3c4e8d32b
Favour-123 <105810406+Favour-123@users.noreply.github.com>
2022-07-01 00:13:21 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - print the string in the put function
 *
@@ -13,6 +14,7 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
