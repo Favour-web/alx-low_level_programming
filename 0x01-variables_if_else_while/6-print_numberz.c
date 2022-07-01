ba80fd157999b7aad42b6f9dfc97a608dae3cb9c
Favour-123 <105810406+Favour-123@users.noreply.github.com>
2022-07-02 01:32:01 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- *        only using putchar and without char variables.
+ * main - Prints the alphabet in lowercase, except for q and e.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int num;
+	int ch;
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	putchar('\n');
+	putchar(10); /* this is an ascii code for new line */
 
 	return (0);
 }
